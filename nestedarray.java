5448a805afaf8ab3ad3b9ab86e0c3abc15673a9a
djmattyg007 <mwgam36@gmail.com>
2013-08-26 22:07:56 +1000

    more nested array

--- nestedarray.java
+++ nestedarray.java
@@ -15,6 +15,23 @@ public class nestedarray
 				System.out.println(rofl[x][y]);
 			}
 		}
+
+		System.out.println("\n\n");
+
+		String[][] rofl2 = new String[3][];
+		for (int x = 0; x < rofl2.length; x++) {
+			String text = "josh" + x;
+			rofl2[x] = new String[x + 1];
+			for (int y = 0; y < rofl2[x].length; y++) {
+				rofl2[x][y] = text + y;
+			}
+		}
+
+		for (int x = 0; x < rofl2.length; x++) {
+			for (int y = 0; y < rofl2[x].length; y++) {
+				System.out.println(rofl2[x][y]);
+			}
+		}
 	}
 }
 
